8bd7de90575ad94d0c9524dda4a6132926095b26
weldonkirui <weldonkirui2000@gmail.com>
2023-08-22 01:06:14 -0700

    0x05-pointers_arrays_strings

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include “main.h”
+/**
+*  _strlen-returns the length of a string
+* @s: string
+*  return: length
+*/
+int  _strlen (char *s)
+{
+         int  longi= o;
+         While (* s I = ‘\0’)
+          {
+              Longi ++;
+              s++;
+            }
+  Return (longi);
+}
+
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,20 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <unistd.h>
+
+int _putchar (char c);
+void reset_to_98(int *n);
+void swap_int (int *a, int *b);
+int _strlen  (char *s);
+void  _puts (char *str);
+void print  _rev (char *s);
+void  rev _string (char *);
+void  puts2 (char *str);
+void  puts_half (char *str);
+void  print_array (int*a, int*b);
+char *_strcpy (char *dest, char *src);
+
+#endif
+
