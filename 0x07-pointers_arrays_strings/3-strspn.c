635099c4c9bf7f62a356a6ea9545ff2b0cb544cd
weldonkirui <weldonkirui2000@gmail.com>
2023-08-29 02:50:40 -0700

    strspn

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -11,10 +11,10 @@ unsigned int _strspn(char *s, char *accept)
 {
 	unsigned int i, j, len = 0, match = 0;
 
-	for (i = 0 ; s[i] != '\0' i++)
+	for (i = 0 ; s[i] != '\0' ; i++)
 
 	{
-		for (j = 0 ; accept[j] != 0 '\0' j++)
+		for (j = 0; accept[j] != '\0'; j++)
 		{
 			if (s[i] == accept[j])
 			{
