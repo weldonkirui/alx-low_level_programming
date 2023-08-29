f100a8ec301275bd2e3468e6fcf802c049860933
weldonkirui <weldonkirui2000@gmail.com>
2023-08-29 02:38:33 -0700

    strchr

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -4,12 +4,12 @@
  * _strchr - a function that locate a character
  * @c: input
  * @s: input string
- * Return: pointer to the first occurrence of the character c in the string s
+ * Return: (0)
  */
 
 char *_strchr(char *s, char c)
 {
-	while (*s != '\0')
+	while (*s >= '\0')
 	{
 		if (*s == c)
 		{
@@ -17,5 +17,5 @@ char *_strchr(char *s, char c)
 		}
 		s++;
 	}
-	return ('\0');
+	return (0);
 }
